d6b475dd7fa254dad8f98dad8dc859d730401a3a
Steveツ <136293871+Steve2495@users.noreply.github.com>
2023-06-13 17:33:47 -0500

    "added Scalable proyect"

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -3,19 +3,20 @@ from pygame.sprite import Sprite
 from game.utils.constants import SPACESHIP, SCREEN_HEIGHT, SCREEN_WIDTH
 
 class Spaceship(Sprite):
+    scale = [40, 60]
     X_POS = ( SCREEN_WIDTH // 2) - 40
-    Y_POS = 500
+    Y_POS = SCREEN_HEIGHT - scale[1]
     
     def __init__(self):
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image, (40, 60))
+        self.image = pygame.transform.scale(self.image, (self.scale[0], self.scale[1]))
         self.rect = self.image.get_rect()
         self.rect.x = self.X_POS
         self.rect.y = self.Y_POS
         
     def move_left(self):
         if self.rect.left == 0:
-            self.rect.x = SCREEN_WIDTH - 30
+            self.rect.x = SCREEN_WIDTH - (self.scale[0] - 10)
         
         if self.rect.left > 0:
             self.rect.x = self.rect.x - 10
@@ -32,7 +33,7 @@ class Spaceship(Sprite):
             self.rect.y = self.rect.y -10        
     
     def move_down(self):
-        if self.rect.y < SCREEN_HEIGHT - 60:
+        if self.rect.y < SCREEN_HEIGHT - self.scale[1]:
             self.rect.y = self.rect.y + 10
     
     def update(self, user_input):
@@ -47,5 +48,8 @@ class Spaceship(Sprite):
     
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
+        
+    def repaint(self, screen):
+        screen.blit(self.image_2, (self.rect.x, self.rect.y))
         
     
